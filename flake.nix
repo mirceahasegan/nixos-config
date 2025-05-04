20b7e6e5b9c10e7604c9ac1918961bc2cc84a4ab
Mircea Hasegan <mircea.hasegan@gmail.com>
2025-05-04 17:14:20 +0200

    Define new systems in a less repetitive way

--- flake.nix
+++ flake.nix
@@ -7,33 +7,26 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
-    nixosConfigurations = {
-      vm-1 = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          ./hosts/vm-1/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "backup";
-            home-manager.users.mircea = import ./shared/home.nix;
-          }
-        ];
-      };
-      desktop-pc = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/desktop-pc/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.mircea = import ./shared/home.nix;
-          }
-        ];
+  outputs = { self, nixpkgs, home-manager }: 
+    let
+      mkHost = hostname: systemType:
+        nixpkgs.lib.nixosSystem {
+          system = systemType;
+          modules = [
+            ./hosts/${hostname}/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+              home-manager.users.mircea = import ./shared/home.nix;
+            }
+          ];
+        };
+    in {
+      nixosConfigurations = {
+        vm-1 = mkHost "vm-1" "aarch64-linux";
+        desktop-pc = mkHost "desktop-pc" "x86_64-linux";
       };
     };
-  };
 }
