420254313ebf3f50f992b7213233ead76b8e8259
Mircea Hasegan <mircea.hasegan@gmail.com>
2025-05-05 18:25:12 +0200

    switch to plasma window manager

--- shared/common.nix
+++ shared/common.nix
@@ -49,8 +49,9 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.defaultSession = "plasma";
 
   # Configure keymap in X11
   services.xserver.xkb = {
