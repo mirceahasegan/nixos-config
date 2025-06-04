3c373175217807bb1ba33335b1a3c6739fad7880
Mircea Hasegan <mircea.hasegan@gmail.com>
2025-06-04 17:23:44 +0200

    add vscode and pcloud for linux

--- shared/home.nix
+++ shared/home.nix
@@ -180,6 +180,8 @@
   ] ++ (if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then [
       slack
       spice-vdagent
+      vscode
+      pcloud
     ] else if pkgs.stdenv.hostPlatform.system == "aarch64-darwin" then [
       iterm2
       utm
